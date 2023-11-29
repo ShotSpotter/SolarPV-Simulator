21879d34c222ec46f5ce0bf100c0327a2bec27d4
Robert B. Calhoun <rcalhoun@shotspotter.com>
2023-11-29 10:20:20 -0500

    General code cleanup

--- SolarPV/NasaData.py
+++ SolarPV/NasaData.py
@@ -29,67 +29,74 @@ Modified on 02/04/2021 to simplify the logic and make better use of Pandas metho
  -------------------------------------------------------------------------------
 """
 
+import datetime as dt
+import json
 import pandas as pd
 import requests
-import datetime as dt
-from collections import namedtuple
 import urllib
-#.parse.urlunsplit, parse.urlencode
-import json
+
+from collections import namedtuple
 
 
-""" BaseURL defines the NASA site used to retrieve Lat/Lon specific data """
+
+""" See https://power.larc.nasa.gov/docs/services/api/ for POWER API"""
 NASA_host = "power.larc.nasa.gov"
-BaseURL = 'https://power.larc.nasa.gov/api/temporal/daily/point'
 
 
 def buildUrl(host:str, path:str, parameters:dict, fragment:str = None) -> str:
-    paramstring = urllib.parse.urlencode(parameters)
-    components=['https', host, path, paramstring, fragment]
+    query_string = urllib.parse.urlencode(parameters)
+    components=['https', host, path, query_string, fragment]
     return urllib.parse.urlunsplit(components)
 
 
 def getLocationData(dtin):
-    """ Retrieves the NASA Location data from the request response 
-        Returns tuple of form (Lon, Lat' Elev)  """
+    """ Retrieves the NASA Location data from the request response
+        Returns tuple of form (Lon, Lat, Elev)  """
     response = json.loads(dtin)
     try:
         coords=response['geometry']['coordinates']
         print(f"found location elevation, z={coords[2]}")
-        return coords
+        # NASA uses -999 as NaN
+        if (coords[2] and coords[2] != -999):
+            return tuple(coords)
+        else:
+            return (None, None, None)
     except KeyError:
         print("Failed to parse elevation response")
-        return [None, None, None]
+        return (None, None, None)
 
 
 def getSiteElevation(lat, lon):
-    # get elevation by asking for temperature 2m above ground level and
-    # pulling elevation from response.geometry.coordinates[3]
-    path = 'api/temporal/daily/point'
-    params = dict()
-    params["parameters"]='T2M'
-    params["start"]='20230101'
-    params["end"]='20230101'
-    params["community"]='sb'
-    params["format"]='json'
-    params["latitude"]=lat
-    params["longitude"]=lon
-    params["user"]='anonymous'
-    url = buildUrl(NASA_host, path, params)
+    """ Request local elevation in meters (datum unspecified, but probably
+    elevation above Mean Sea Level) from NASA POWER API version 2. Following
+    a NASA example, the request queries parameter "T2M" (Temperature 2 meters
+    above ground level, then parses only the [geometry] portion of the the
+    response.
+    """
+    apipath = '/api/temporal/daily/point'
+    query_parameters = dict()
+    query_parameters["parameters"]='T2M'
+    # use fixed historical date to avoid missing current data
+    query_parameters["start"]="20230101"
+    query_parameters["end"]="20230101"
+    query_parameters["community"]='sb'
+    query_parameters["format"]='json'
+    query_parameters["latitude"]=lat
+    query_parameters["longitude"]=lon
+    query_parameters["user"]='solarpv_simulator'
+    url = buildUrl(NASA_host, apipath, query_parameters)
     # Request NASA Data from API
     print(f"Trying to get data from NASA at {url}")
     try:
         data = requests.get(url).text
         return getLocationData(data)
     except requests.exceptions.ConnectionError:
-        return [None, None, None]
+        return (None, None, None)
     
 def formulateRequest(lat, lon, selectparms= None):
     """ Formulate a request from NASA API for 10 years of atmospheric data 
         required to prepare daily statistical data used in Solar Insolation
         calculations """
-    baseURL = BaseURL
-    baseReq = 'parameters='
     stdparms = [('T10M','Temperature @ 10m (c)'),
                 ('T10M_MAX', 'Max Daily Temperature (c)'),
                 ('T10M_MIN', 'Min Daily Temperature (c)'),
@@ -106,23 +113,20 @@ def formulateRequest(lat, lon, selectparms= None):
     for itm in stdparms:
         if selectparms == None or itm[0] in selectparms:
             parms.append(itm[0])
-    reqparms = ''
-    for p in range(len(parms)):
-        if p > 0:
-            reqparms += ','
-        reqparms += parms[p]
+    # to csv
+    reqparms = ",".join(str(param) for param in parms)
 
-    path = 'api/temporal/daily/point'
-    params = dict()
-    params["parameters"]=reqparms
-    params["start"]=startdate
-    params["end"]=enddate
-    params["community"]='sb'
-    params["format"]='json'
-    params["latitude"]=lat
-    params["longitude"]=lon
-    params["user"]='anonymous'
-    url = buildUrl(NASA_host, path, params)
+    apipath = '/api/temporal/daily/point'
+    query_parameters = dict()
+    query_parameters["parameters"]=reqparms
+    query_parameters["start"]=startdate
+    query_parameters["end"]=enddate
+    query_parameters["community"]='sb'
+    query_parameters["format"]='json'
+    query_parameters["latitude"]=lat
+    query_parameters["longitude"]=lon
+    query_parameters["user"]='anonymous'
+    url = buildUrl(NASA_host, apipath, query_parameters)
     return (url, reqparms.split(','))
 
 
